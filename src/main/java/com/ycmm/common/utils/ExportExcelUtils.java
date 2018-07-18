fb7a2741374d6015c6d84373a9ca6464bbb8963f
qinyanguo <2155483971@qq.com>
2018-07-18 14:57:04 +0800

    Excel导出  添加标题

--- src/main/java/com/ycmm/common/utils/ExportExcelUtils.java
+++ src/main/java/com/ycmm/common/utils/ExportExcelUtils.java
@@ -2,6 +2,7 @@ package com.ycmm.common.utils;
 
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 
 import java.io.File;
@@ -17,8 +18,25 @@ public class ExportExcelUtils {
 
     public static void exportExcel(String title, String fileName, String[] headers, String[] cellName,
                                    List<Map<String, Object>> dataList, OutputStream os) {
+
         // 声明一个工作薄
         HSSFWorkbook workbook = new HSSFWorkbook();
+
+        //----------------标题样式---------------------
+        HSSFCellStyle titleStyle = workbook.createCellStyle();        //标题样式
+        titleStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        titleStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+        HSSFFont ztFont = workbook.createFont();
+        ztFont.setItalic(false);                     // 设置字体为斜体字
+        ztFont.setColor(HSSFFont.COLOR_NORMAL);            // 将字体设置为“红色”
+        ztFont.setFontHeightInPoints((short)18);    // 将字体大小设置为18px
+        ztFont.setFontName("宋体");             // 将“宋体”字体应用到当前单元格上
+        ztFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);    //加粗
+//      ztFont.setUnderline(Font.U_DOUBLE);         // 添加（Font.U_SINGLE单条下划线/Font.U_DOUBLE双条下划线）
+//      ztFont.setStrikeout(true);                  // 是否添加删除线
+        titleStyle.setFont(ztFont);
+
+
         // 生成一个表格
         HSSFSheet sheet = workbook.createSheet(title);
         // 设置表格默认列宽度为15个字节
@@ -57,7 +75,7 @@ public class ExportExcelUtils {
         style2.setFont(font2);
         // 产生表格标题行
 
-        setCell(sheet, headers, cellName, style, style2, dataList);
+        setCell(sheet, title, headers, cellName, style, style2, titleStyle, dataList);
 
         try {
             File file = new File("D:/poi", fileName);
@@ -76,6 +94,21 @@ public class ExportExcelUtils {
                                    List<Map<String, Object>> dataList) {
         // 声明一个工作薄
         HSSFWorkbook workbook = new HSSFWorkbook();
+
+        //----------------标题样式---------------------
+        HSSFCellStyle titleStyle = workbook.createCellStyle();        //标题样式
+        titleStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        titleStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+        HSSFFont ztFont = workbook.createFont();
+        ztFont.setItalic(false);                     // 设置字体为斜体字
+        ztFont.setColor(HSSFFont.COLOR_NORMAL);            // 将字体设置为“红色”
+        ztFont.setFontHeightInPoints((short)18);    // 将字体大小设置为18px
+        ztFont.setFontName("宋体");             // 将“宋体”字体应用到当前单元格上
+        ztFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);    //加粗
+//      ztFont.setUnderline(Font.U_DOUBLE);         // 添加（Font.U_SINGLE单条下划线/Font.U_DOUBLE双条下划线）
+//      ztFont.setStrikeout(true);                  // 是否添加删除线
+        titleStyle.setFont(ztFont);
+
         // 生成一个表格
         HSSFSheet sheet = workbook.createSheet(title);
         // 设置表格默认列宽度为15个字节
@@ -114,7 +147,7 @@ public class ExportExcelUtils {
         style2.setFont(font2);
         // 产生表格标题行
 
-        setCell(sheet, headers, cellName, style, style2, dataList);
+        setCell(sheet, title, headers, cellName, style, style2, titleStyle, dataList);
 
         try {
             File file = new File("F:\\Demo\\project\\Qin\\target\\qin", fileName);
@@ -128,19 +161,31 @@ public class ExportExcelUtils {
     }
 
 
-    private static void setCell( HSSFSheet sheet, String[] headers, String[] cellName, HSSFCellStyle style,
-                                HSSFCellStyle style2,
-                          List<Map<String, Object>> dataList) {
-        HSSFRow row = sheet.createRow(0);
+    private static void setCell( HSSFSheet sheet, String title, String[] headers, String[] cellName, HSSFCellStyle style,
+                                HSSFCellStyle style2, HSSFCellStyle titleStyle, List<Map<String, Object>> dataList) {
+
+        // ----------------------创建第一行---------------
+        // 在sheet里创建第一行，参数为行索引(excel的行)，可以是0～65535之间的任何一个
+        HSSFRow titleRow = sheet.createRow(0);
+        // 创建单元格（excel的单元格，参数为列索引，可以是0～255之间的任何一个
+        HSSFCell titleCell = titleRow.createCell(0);
+        // 合并单元格CellRangeAddress构造参数依次表示起始行，截至行，起始列， 截至列
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headers.length-1));
+        // 设置单元格内容
+        titleCell.setCellValue(title);
+        titleCell.setCellStyle(titleStyle);
+
+        HSSFRow row = sheet.createRow(1);
         for (short i = 0; i < headers.length; i++) {
             HSSFCell cell = row.createCell(i);
             cell.setCellStyle(style);
             HSSFRichTextString text = new HSSFRichTextString(headers[i]);
             cell.setCellValue(text);
         }
+
         String value = null;
         for (int i = 0, j = dataList.size(); i < j; i++) {
-            HSSFRow rowc = sheet.createRow(i + 1);
+            HSSFRow rowc = sheet.createRow(i + 2);
             Map<String, Object> map = dataList.get(i);
             for (int m = 0, n = headers.length; m < n; m++) {
                 HSSFCell cell = rowc.createCell(m);
